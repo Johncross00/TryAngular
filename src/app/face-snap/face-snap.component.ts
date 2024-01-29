f54c8ba83eb0a3f7f4aba366aee8eb77a818f4b2
johncross00 <johncross.ktm@gmail.com>
2024-01-29 00:19:41 +0000

    add data

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+
+  ngOnInit(): void {
+      this.title = 'Germain';
+      this.description = 'My best friend';
+      this.createdDate = new Date();
+      this.snaps = 6;
+  }
 
 }
